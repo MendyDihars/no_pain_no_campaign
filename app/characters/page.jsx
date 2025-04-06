924dd9dd35eb5ad5e03071fd29b5bd7f4e4c5a87
Mendy <mendy.dihars@laposte.net>
2025-04-06 18:54:53 +0200

    Ajout de la documentation pour la base de données NoPainNoCampaign, mise à jour des modèles de données et des migrations, et refactorisation de la logique de récupération des personnages. Suppression de composants obsolètes et amélioration de la structure des tables dans la base de données.

--- app/characters/page.jsx
+++ app/characters/page.jsx
@@ -1,18 +1,11 @@
-import Character from '@root/lib/models/character';
 import CharacterCard from '@root/components/CharacterCard';
+import { DB } from '@root/lib/db';
 
 export default async function CharactersPage() {
-  const characters = await Character.select('*').orderBy('firstname', 'asc');
+  const characters = await DB.characters.select('*').orderBy('firstname', 'asc');
 
   return (
     <div>
-
-
-      <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-6 p-6">
-        {characters.map((character) => (
-          <CharacterCard key={character.id} character={character} />
-        ))}
-      </div>
     </div>
   );
 }
